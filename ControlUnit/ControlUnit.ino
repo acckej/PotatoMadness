96356b41160e61708d11b40e67237c485e728b44
Acckej <goblincorp@yandex.ru>
2017-08-16 23:35:32 +0300

    Initial commit

---
+++ ControlUnit/ControlUnit.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		ControlUnit.ino
+ Created:	8/16/2017 11:21:32 PM
+ Author:	Acckej
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
+
