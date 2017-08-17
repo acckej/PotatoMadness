fa9f63ead9b639b5002f697f4adb2cf8348d6b9c
Acckej <goblincorp@yandex.ru>
2017-08-17 19:57:39 +0300

    initial commit

--- ControlUnit/ControlUnit.ino
+++ ControlUnit/ControlUnit.ino
@@ -3,6 +3,7 @@
  Created:	8/16/2017 11:21:32 PM
  Author:	Acckej
 */
+#include "HwCheckSequence.h"
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -11,6 +12,6 @@ void setup() {
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	auto a = HwCheckSequence();	
 }
 
